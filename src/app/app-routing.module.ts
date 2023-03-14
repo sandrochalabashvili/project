4522db63a644afe9d4fdda381cab93b067cc2ec8
sandrolakhteevi <sandro.chalabashvili031@gmail.com>
2023-03-14 15:59:52 +0400

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './view/about/about.component';
+import { HomepageComponent } from './view/homepage/homepage.component';
+import { NotfoundComponent } from './view/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomepageComponent,},
+  {path:"about",component:AboutComponent,},
+  {path:"404",component:NotfoundComponent,},
+  {path:"**",pathMatch:"full",redirectTo:"404"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+
+import { HomepageComponent } from './view/homepage/homepage.component';
+import { NotfoundComponent } from './view/notfound/notfound.component';
+import { AboutComponent } from './view/about/about.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NotfoundComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
